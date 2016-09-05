8dbaaf380919504a0317ee9e70c7609dbd77985f
coachmaxz <eakkabin_it@outlook.co.th>
2016-09-06 01:12:55 +0700

    #6 add command example

--- app/components/AppComponent.jsx
+++ app/components/AppComponent.jsx
@@ -116,6 +116,86 @@ class AppComponent extends React.Component {
 
     // console.log(this.state);
 
+    /*
+    // สร้างจุด Mark
+    var marker = new maps.Marker({
+      position: { lat: 13.883165, lng: 100.515570 }, // Position ที่ต้องการ
+      map: this.map,
+      title: 'Click to zoom'
+    });
+    // กรณี Click ให้ Zoom เท่ากับ 8
+    marker.addListener('click', () => {
+      this.map.setZoom(8); // Zoom เท่ากับ 8
+      this.map.setCenter(marker.getPosition()); // จัด Center ให้กับ Position ที่ต้องการ
+    });
+    // กรณี มีการเปลี่ยนตำแหน่ง Center ให้กลับไปยังตำแหน่งเดิม
+    marker.addListener('center_changed', () => {
+      window.setTimeout(() => {
+        this.map.panTo(marker.getPosition());
+      }, 3000); // ภายใน 3 วินาที
+    });
+    */
+
+    /*
+    // สร้างจุด Mark (ตำแหน่งปัจจุบัน)
+    var infoWindow = new maps.InfoWindow({ map: this.map });
+    var handleLocationError = (browserHasGeolocation, infoWindow, position) => {
+      infoWindow.setPosition(position);
+      infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+    }
+    // ดึง Location ปัจจุบัน (สนับสนุน HTML5)
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        var position = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        infoWindow.setPosition(position);
+        infoWindow.setContent('คุณกำลังอยู่ที่นี่!'); // ข้อความแสดงจุด Mark
+        this.map.setZoom(13); // Zoom เท่ากับ 13
+        this.map.setCenter(position);  // จัด Center ให้กับ Position ที่ต้องการ
+        }, () => {
+            handleLocationError(true, infoWindow, this.map.getCenter());
+        });
+      } else {
+        handleLocationError(false, infoWindow, this.map.getCenter());
+    }
+    */
+
+    /*
+    // ย้ายจุด Mark
+    var marker = new maps.Marker({
+      position: { lat: 13.7246005, lng: 100.6331108 }, // Position ที่ต้องการ
+      map: this.map,
+      title: 'Click to zoom'
+    });
+    // กรณี Click ให้ Zoom เท่ากับ 8
+    marker.addListener('click', () => {
+      this.map.setZoom(8); // Zoom เท่ากับ 8
+      this.map.setCenter(marker.getPosition()); // จัด Center ให้กับ Position ที่ต้องการ
+    });
+    */
+
+    /*
+    // แสดงตัวเลข Zoom (ปัจจุบัน)
+    let center = new maps.LatLng(this.state.setting.initialCenter.lat, this.state.setting.initialCenter.lng);
+    var infowindow = new maps.InfoWindow({
+      content: 'ทดสอบการ Zoom',
+      position: center
+    });
+    // กรณี แสดง Infomation อัตโนมัติ
+    infowindow.open(this.map); // แสดง Infomation
+    // กรณี แสดง Infomation เมื่อมีการ Click
+    this.map.addListener('click', () => {
+        infowindow.open(this.map); // แสดง Infomation
+    });
+    // กรณี มีการเปลี่ยนแปลงค่า Zoom (ปัจจุบัน)
+    this.map.addListener('zoom_changed', () => {
+      infowindow.setContent('Zoom: ' + this.map.getZoom()); // แสดงตัวเลข Zoom (ปัจจุบัน)
+    });
+
+    */
+
   }
 
   handleEvent(evtName) {
